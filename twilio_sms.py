d91936b5b19d57454ae2334539f71fbf9f6f22f8
Abylin Johnson <81345003+abylinjohnson@users.noreply.github.com>
2022-01-13 13:46:26 +0530

    Update twilio_sms.py

--- twilio_sms.py
+++ twilio_sms.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
 
-account_sid = "AC19b46169d02b236d553d811b00e53b54"
-account_token = "5ef95754e3a23280ef75091057cf851e"
+account_sid = "your sid"
+account_token = "your auth token"
 client = Client(account_sid, account_token)
 
 message = client.messages.create(
     body='Good Evening My Friend!',
-    from_='+16085120530',
-    to='+919488588505'
+    from_='from number',
+    to='to number'
 )
 
 print(message.sid)
